e693cca4a73488dba8191f3d5c68890a83dc1d28
Alan Fluder <afluder1@mix.wvu.edu>
2015-04-12 18:55:24 -0400

    comments

--- earthQuakeJava+Bash/Driver.java
+++ earthQuakeJava+Bash/Driver.java
@@ -4,34 +4,66 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
+/**
+ * Prints strongest earthquake, within 100 miles of Interana's office that
+ * occurred in the last 7 days. Pre-req is a trimmed json earthquake data file
+ * which can be generated using the getData.sh script.
+ * 
+ * @author Alan Fluder
+ */
 public class Driver {
-	
-	private static Double maxMagnitude=0.0;
+
+	private static Double maxMagnitude = 0.0;
 	private static Earthquake highestMagnitudeEarthquake;
-    public static void main(String[] args) {
-    	File rawJSON = new File("/tmp/trimmedEarthquakeData");
-    	
+
+	public static void main(String[] args) {
+		new Driver().parseFile();
+	}
+
+	/**
+	 * Cycles through earthquakes
+	 */
+	public void parseFile() {
+		Scanner scanner = makeScannerFor("/tmp/trimmedEarthquakeData");
+		while (scanner.hasNext()) {
+			Earthquake newEarthquake = new Earthquake(scanner.nextLine());
+			checkIfValidEarthquake(newEarthquake);
+		}
+		scanner.close();
+		System.out.println(highestMagnitudeEarthquake.toString());
+	}
+
+	/**
+	 * Allows parsing of specified file
+	 * 
+	 * @param path
+	 *            to data file
+	 */
+	public Scanner makeScannerFor(String dataFileName) {
+		File jsonFile = new File(dataFileName);
+		Scanner scanner = null;
 		try {
-			Scanner scanner = new Scanner(rawJSON);
-    	while (scanner.hasNext()) {	
-    		String earthquakeDataElements = scanner.nextLine();
-    		Earthquake newEarthquake = new Earthquake(earthquakeDataElements);
-    	
-		if (newEarthquake.getMagnitude() > maxMagnitude){
-			if (newEarthquake.lessThanWeekOld()) {
-				if (newEarthquake.getDistance() < 100) {
-    			maxMagnitude = newEarthquake.getMagnitude();
-    			highestMagnitudeEarthquake = newEarthquake;
-				}
-    		}
-    	}
-    	}
-    	scanner.close();
+			scanner = new Scanner(jsonFile);
 		} catch (FileNotFoundException e) {
 			System.out.println("Data file not found");
 		}
-		System.out.println(highestMagnitudeEarthquake.toString());
-    }
-}
+		return scanner;
+	}
 
+	/**
+	 * Checks magnitude, age and distance of earthquake
+	 * 
+	 * @param Custom
+	 *            object for earthquakes
+	 */
+	public void checkIfValidEarthquake(Earthquake newEarthquake) {
+		if (newEarthquake.getMagnitude() > maxMagnitude) {
+			if (newEarthquake.lessThanWeekOld()) {
+				if (newEarthquake.getDistance() < 100) {
+					maxMagnitude = newEarthquake.getMagnitude();
+					highestMagnitudeEarthquake = newEarthquake;
+				}
+			}
+		}
+	}
+}
--- earthQuakeJava+Bash/Earthquake.java
+++ earthQuakeJava+Bash/Earthquake.java
@@ -1,5 +1,10 @@
 package commands;
 
+/**
+ * Earthquake objects from JSON file
+ * 
+ * @author Alan Fluder
+ */
 public class Earthquake {
 	private String magnitude;
 	private String dateStamp;
@@ -12,36 +17,55 @@ public class Earthquake {
 		dateStamp = rawDataElements[1];
 		latitude = rawDataElements[2];
 		longitude = rawDataElements[3];
-		}
-		
+	}
+
+	/**
+	 * Compares UNIX timestamps (seconds since 1/1/1970)
+	 */
 	public boolean lessThanWeekOld() {
 		final int singleWeekInSeconds = 604800;
 		long unixTimeStamp = System.currentTimeMillis() / 1000L;
 		long oneWeekAgo = unixTimeStamp - singleWeekInSeconds;
 		return oneWeekAgo < Long.parseLong(dateStamp);
-}
-	
+	}
+
 	public Double getMagnitude() {
 		return Double.parseDouble(magnitude);
 	}
-	public Double getDistance() {   
-	    final double earthRadius = 3958.76;
-	    final double interanaLatitude = 37.452234;
-		    final double interanaLongitude = -122.166213;
-		    
-		    double earthquakeLatitude = Double.parseDouble(latitude);
-		    double earthquakeLongitude = Double.parseDouble(longitude);
-		    double deltaLatitude = interanaLatitude - earthquakeLatitude;
-		    double deltaLongitude = interanaLongitude - earthquakeLongitude;
-		    
-		    double distanceBetweenPoints = Math.sin(Math.toRadians(deltaLatitude) / 2) * Math.sin(Math.toRadians(deltaLatitude) / 2) 
-		                            + Math.sin(Math.toRadians(deltaLongitude) / 2) * Math.sin(Math.toRadians(deltaLongitude) / 2) 
-		                            * Math.cos(Math.toRadians(interanaLatitude)) * Math.cos(Math.toRadians(earthquakeLatitude));
-		    distanceBetweenPoints = 2 * Math.asin(Math.sqrt(distanceBetweenPoints));
-		    distanceBetweenPoints = earthRadius * distanceBetweenPoints;
-		    return distanceBetweenPoints;
-		 }
+
+	/**
+	 * Representation of Haversine formula for finding distance between two
+	 * lat/longs
+	 * 
+	 * @return Distance between two points in miles
+	 */
+	public Double getDistance() {
+		final double earthRadius = 3958.76;
+		final double interanaLatitude = 37.452234;
+		final double interanaLongitude = -122.166213;
+
+		double earthquakeLatitude = Double.parseDouble(latitude);
+		double earthquakeLongitude = Double.parseDouble(longitude);
+		double deltaLatitude = interanaLatitude - earthquakeLatitude;
+		double deltaLongitude = interanaLongitude - earthquakeLongitude;
+
+		double distanceBetweenPoints = Math
+				.sin(Math.toRadians(deltaLatitude) / 2)
+				* Math.sin(Math.toRadians(deltaLatitude) / 2)
+				+ Math.sin(Math.toRadians(deltaLongitude) / 2)
+				* Math.sin(Math.toRadians(deltaLongitude) / 2)
+				* Math.cos(Math.toRadians(interanaLatitude))
+				* Math.cos(Math.toRadians(earthquakeLatitude));
+		distanceBetweenPoints = 2 * Math.asin(Math.sqrt(distanceBetweenPoints));
+		distanceBetweenPoints = earthRadius * distanceBetweenPoints;
+		return distanceBetweenPoints;
+	}
+
+	/**
+	 * Custom printout for user
+	 */
 	public String toString() {
-		return "Magnitude: "+magnitude+" UNIX Time: "+dateStamp+" Latitude/Longitude: "+latitude+" "+longitude;
+		return "Magnitude: " + magnitude + " UNIX Time: " + dateStamp
+				+ " Latitude/Longitude: " + latitude + ", " + longitude;
 	}
-	}
+}
